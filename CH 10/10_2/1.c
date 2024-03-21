40bfcb08051062559b060e856b36df6dfa4ae3ab
Riya-davra04 <riyadavra@gmail.com>
2024-03-21 23:16:06 +0530

    21/03/2024

---
+++ CH 10/10_2/1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int Arraysum(int a[] , int n)
+{
+    int sum=0;
+
+    for(int i=0 ; i< n; i++)
+    {
+        sum += a[i];
+
+    }
+
+    return sum;
+}
+
+void main()
+{
+    int n;
+    printf("Enter array size : ");
+    scanf("%d",&n);
+
+    int a[n];
+
+    for(int i=0 ; i<n ;i++)
+    {
+        printf("a[%d] : ",i);
+        scanf("%d", &a[i]);
+    }
+
+    int sum = Arraysum(a,n);
+    printf("Sum : %d\n",sum);
+
+}
